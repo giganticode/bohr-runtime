1b1de574ee0c562f95030581fb13f87e0841c1be
hlib <hlibbabii@gmail.com>
2021-03-09 15:49:40 +0100

    fixed labeled datasets columns for all tasks

--- bohr/pipeline/core.py
+++ bohr/pipeline/core.py
@@ -1,7 +1,11 @@
+from typing import List
+
 import numpy as np
 import pandas as pd
 from pandas import Series
 from snorkel.labeling.model import LabelModel
+
+from bohr.labels.labelset import Label
 
 
 def train_lmodel(lines_train: np.ndarray) -> LabelModel:
@@ -11,13 +15,16 @@ def train_lmodel(lines_train: np.ndarray) -> LabelModel:
 
 
 def label(
-    label_model: LabelModel, matrix: np.ndarray, df: pd.DataFrame
+    label_model: LabelModel,
+    matrix: np.ndarray,
+    df: pd.DataFrame,
+    label_names: List[str],
 ) -> pd.DataFrame:
     labels, probs = label_model.predict(L=matrix, return_probs=True)
     probs = np.around(probs, decimals=2)
-    df_labeled = df.assign(bug_predicted=Series(labels))
+    df_labeled = df.assign(predicted=Series(labels))
 
-    df_labeled["prob_bugless"] = Series(probs[:, 0])
-    df_labeled["prob_bug"] = Series(probs[:, 1])
-    df_labeled["bug_class"] = Series(np.around(np.copy(probs[:, 1]), decimals=1))
+    df_labeled[f"prob_{label_names[0]}"] = Series(probs[:, 0])
+    df_labeled[f"prob_{label_names[1]}"] = Series(probs[:, 1])
+    df_labeled["prob_class"] = Series(np.around(np.copy(probs[:, 1]), decimals=1))
     return df_labeled
