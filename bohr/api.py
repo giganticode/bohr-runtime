ea1bebf8586a4c947fb9b6680c6f032fe3127961
hlib <hlibbabii@gmail.com>
2021-03-30 17:56:53 +0200

    fix: update lock when updating config

--- bohr/api.py
+++ bohr/api.py
@@ -12,12 +12,12 @@ def refresh() -> None:
     config = load_config()
     (config.paths.project_root / "dvc.yaml").unlink(missing_ok=True)
     add_all_tasks_to_dvc_pipeline(config)
+    update_lock(config.paths)
 
 
 def refresh_if_necessary(path_config: PathConfig) -> None:
     if not bohr_up_to_date(path_config):
         logger.info("There are changes to the bohr config. Refreshing the workspace...")
         refresh()
-        update_lock(path_config)
     else:
         logger.info("Bohr config hasn't changed.")
