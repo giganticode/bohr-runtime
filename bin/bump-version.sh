69065a1261f15d1848e9f4bf90b2ab59a2e12204
hlib <hlibbabii@gmail.com>
2021-04-03 12:34:02 +0200

    bump script: possibility to specify minor or major version bump

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -16,12 +16,26 @@ pip install jq===1.1.2 -qqq
 BOHR_VERSION="$( cat $version_file )"
 echo "Current bohr version is $BOHR_VERSION"
 
-if ! [[ "$BOHR_VERSION" =~ .*-rc ]]; then
-    echo "Version in bohr/VERSION file should match regex: .*-rc"
+if ! [[ "$BOHR_VERSION" =~ [0-9]+\\.[0-9]+\\.[0-9]+-rc ]]; then
+    echo "Version in bohr/VERSION file should match regex: [0-9]+\.[0-9]+\.[0-9]+-rc"
     exit 3
 fi
 
-tag_bohr_version="$(echo $BOHR_VERSION | sed -e 's/-rc$//g' )"
+version_wo_rc="$(echo $BOHR_VERSION | sed -e 's/-rc$//g' )"
+patch_version="$(echo "$version_wo_rc" | sed -E 's/^([0-9]+)\.([0-9]+)\.([0-9]+)$/\3/g' )"
+minor_version="$(echo "$version_wo_rc" | sed -E 's/^([0-9]+)\.([0-9]+)\.([0-9]+)$/\2/g' )"
+major_version="$(echo "$version_wo_rc" | sed -E 's/^([0-9]+)\.([0-9]+)\.([0-9]+)$/\1/g' )"
+
+if [[ ${1:-patch} == "minor" ]]; then
+    minor_version=$(echo "$minor_version+1" | bc)
+    patch_version=0
+elif [[ ${1:-patch} == "major" ]]; then
+    major_version=$(echo "$major_version+1" | bc)
+    minor_version=0
+    patch_version=0
+fi
+
+tag_bohr_version="$major_version.$minor_version.$patch_version"
 
 jq ".bohr_framework_version = \"$tag_bohr_version\"" "$bohr_config_file" > "${bohr_config_file}.tmp"
 mv -f "${bohr_config_file}.tmp" "$bohr_config_file"
@@ -34,7 +48,6 @@ git commit -am "Bump version to $tag"
 git tag -a "$tag" -m "$tag"
 git push --tags
 
-patch_version="$(echo "$tag_bohr_version" | sed -E 's/.*\.([0-9]+)$/\1/g' )"
 incremented_patch_version=$(echo "$patch_version+1" | bc )
 echo $tag_bohr_version | sed -E "s/[0-9]+$/$incremented_patch_version-rc/g" > "$version_file"
 
