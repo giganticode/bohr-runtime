0f76607c3fa96359d0743c4c11b81f6b46f50ac1
hlib <hlibbabii@gmail.com>
2021-05-06 14:38:57 +0200

    reduce number of keywords

--- test-b2b/scenario1/heuristics/bugginess/main_heurstics.py
+++ test-b2b/scenario1/heuristics/bugginess/main_heurstics.py
@@ -5,7 +5,7 @@ from labels import *
 
 from bohr.artifacts.commit import Commit
 from bohr.decorators import Heuristic
-from bohr.labels.labelset import Label, Labels
+from bohr.labels.labelset import Labels
 from bohr.nlp_utils import NgramSet
 from bohr.templates.heuristics.keywords import KeywordHeuristics
 
@@ -14,164 +14,28 @@ from bohr.templates.heuristics.keywords import KeywordHeuristics
     Commit,
     keywords=[
         "bad",
-        "broken",
         ["bug", "bugg"],
-        "close",
-        "concurr",
-        ["correct", "correctli"],
-        "corrupt",
-        "crash",
-        ["deadlock", "dead lock"],
         "defect",
-        "disabl",
-        "endless",
-        "ensur",
-        "error",
-        "except",
         ["fail", "failur", "fault"],
         ["bugfix", "fix", "hotfix", "quickfix", "small fix"],
-        "garbag",
-        "handl",
-        "incomplet",
-        "inconsist",
-        "incorrect",
-        "infinit",
-        "invalid",
-        "issue",
-        "leak",
-        "loop",
-        "minor",
-        "mistak",
-        ["nullpointer", "npe", "null pointer"],
         "not work",
-        "not return",
         ["outofbound", "of bound"],
-        "patch",
-        "prevent",
-        "problem",
-        "properli",
-        "race condit",
-        "repair",
-        ["resolv", "solv"],
-        ["threw", "throw"],
-        "timeout",
-        "unabl",
-        "unclos",
-        "unexpect",
-        "unknown",
-        "unsynchron",
-        "wrong",
     ],
-    name_pattern="bug_message_keyword_%1",
+    name_pattern="bug_message_keywords",
+    lf_per_key_word=False,
 )
 def bug_keywords_lookup_in_message(
-    commit: Commit, keywords: NgramSet
+    commit: Commit, keywords: List[NgramSet]
 ) -> Optional[Labels]:
-    if commit.message.match_ngrams(keywords):
-        return CommitLabel.BugFix
+    for keyword in keywords:
+        if commit.message.match_ngrams(keyword):
+            return CommitLabel.BugFix
     return None
 
 
 @KeywordHeuristics(
     Commit,
-    keywords=[
-        "abil",
-        "ad",
-        "add",
-        "addit",
-        "allow",
-        "analysi",
-        "avoid",
-        "baselin",
-        "beautification",
-        "benchmark",
-        "better",
-        "bump",
-        "chang log",
-        ["clean", "cleanup"],
-        "comment",
-        "complet",
-        "configur chang",
-        "consolid",
-        "convert",
-        "coverag",
-        "create",
-        "deprec",
-        "develop",
-        ["doc", "document", "javadoc"],
-        "drop",
-        "enhanc",
-        "exampl",
-        "exclud",
-        "expand",
-        "extendgener",
-        "featur",
-        "forget",
-        "format",
-        "gitignor",
-        "idea",
-        "implement",
-        "improv",
-        "includ",
-        "info",
-        "intorduc",
-        "limit",
-        "log",
-        "migrat",
-        "minim",
-        "modif",
-        "move",
-        "new",
-        "note",
-        "opinion",
-        ["optim", "optimis"],
-        "pass test",
-        "perf test",
-        "perfom test",
-        "perform",
-        "plugin",
-        "polish",
-        "possibl",
-        "prepar",
-        "propos",
-        "provid",
-        "publish",
-        "readm",
-        "reduc",
-        "refin",
-        "reformat",
-        "regress test",
-        "reimplement",
-        "release",
-        "remov",
-        "renam",
-        "reorgan",
-        "replac",
-        "restrict",
-        "restructur",
-        "review",
-        "rewrit",
-        "rid",
-        "set up",
-        "simplif",
-        "simplifi",
-        ["speedup", "speed up"],
-        "stage",
-        "stat",
-        "statist",
-        "support",
-        "switch",
-        "test",
-        "test coverag",
-        "test pass",
-        "todo",
-        "tweak",
-        "unit",
-        "unnecessari",
-        "updat",
-        "upgrad",
-        "version",
-    ],
+    keywords=["add", ["doc", "document", "javadoc"]],
     name_pattern="bugless_message_keyword_%1",
 )
 def bugless_keywords_lookup_in_message(
@@ -189,40 +53,11 @@ def bugless_keywords_lookup_in_message(
     lf_per_key_word=False,
 )
 def refactoring_keywords_lookup_in_message(
-        commit: Commit,
-        keywords: List[NgramSet],
+    commit: Commit,
+    keywords: List[NgramSet],
 ) -> Optional[Labels]:
     if commit.message.match_ngrams(keywords[0]):
         return CommitLabel.NonBugFix
-    return None
-
-
-# @KeywordHeuristics(
-#     Commit,
-#     keywords=[
-#         "ad",
-#         "add",
-#         "build",
-#         "chang",
-#         "doc",
-#         "document",
-#         "javadoc",
-#         "junit",
-#         "messag",
-#         "report",
-#         "test",
-#         "typo",
-#         "unit",
-#         "warn",
-#     ],
-#     name_pattern="bogusbugs_message_keyword_%1",
-# )
-def bogus_fix_keyword_in_message(commit: Commit, keywords: NgramSet) -> Optional[Label]:
-    if "fix" in commit.message.stemmed_ngrams or "bug" in commit.message.stemmed_ngrams:
-        if commit.message.match_ngrams(keywords):
-            return CommitLabel.NonBugFix
-        else:
-            return CommitLabel.BugFix
     return None
 
 
