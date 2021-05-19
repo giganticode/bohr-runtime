07213f8c3866a7c8d34bf1839c05f3a59de73ef9
hlib <hlibbabii@gmail.com>
2021-05-19 14:18:11 +0200

    bohr task add -v flag

--- bohr/cli/task/commands.py
+++ bohr/cli/task/commands.py
@@ -6,10 +6,11 @@ from bohr import api
 from bohr.collection.artifacts import artifact_map
 from bohr.config.pathconfig import PathConfig
 from bohr.datamodel.bohrrepo import load_bohr_repo
-from bohr.datamodel.dataset import get_all_linked_datasets, train_and_test
+from bohr.datamodel.dataset import train_and_test
 from bohr.datamodel.heuristic import get_heuristic_module_list
 from bohr.datamodel.task import Task
 from bohr.fs import find_project_root
+from bohr.util.logging import verbosity
 
 logger = logging.getLogger(__name__)
 
@@ -50,6 +51,7 @@ def add_dataset(task: str, dataset: str, repro: bool) -> None:
 @click.option("-A", "--use-all-datasets", is_flag=True)
 @click.option("--repro", is_flag=True)
 @click.option("--force", is_flag=True)
+@click.option("-v", "--verbose", is_flag=True, help="Enables verbose mode")
 def add(
     name: str,
     artifact: str,
@@ -60,42 +62,46 @@ def add(
     use_all_datasets: bool,
     repro: bool,
     force: bool,
+    verbose: bool,
 ) -> None:
-    project_root = find_project_root()
-    bohr_repo = load_bohr_repo(project_root)
-    path_config = PathConfig.load(project_root)
-    if name in bohr_repo.tasks and not force:
-        logger.error(f"Task {name} is already defined")
-        exit(400)
-    try:
-        artifact_type = artifact_map[artifact]
-    except KeyError:
-        logger.error(f"Artifact not found: {artifact}")
-        exit(404)
-    label_list = list(map(lambda s: s.strip(), labels.split(",")))
-    if not use_all_datasets:
-        train_datasets, test_datasets = {}, {}
-    else:
-        all_datasets = {
-            n: d
-            for n, d in bohr_repo.datasets.items()
-            if d.artifact_type == artifact_type
-        }
-        train_datasets, test_datasets = train_and_test(all_datasets, label_column)
-    heuristic_groups = get_heuristic_module_list(artifact_type, path_config.heuristics)
-    task = Task(
-        name,
-        authors,
-        description,
-        artifact_type,
-        label_list,
-        train_datasets,
-        test_datasets,
-        label_column,
-        heuristic_groups,
-    )
-    bohr_repo.tasks[name] = task
-    bohr_repo.dump(project_root)
-    if repro:
-        logger.info("Re-running the pipeline ...")
-        api.repro(name, bohr_repo=bohr_repo)
+    with verbosity(verbose):
+        project_root = find_project_root()
+        bohr_repo = load_bohr_repo(project_root)
+        path_config = PathConfig.load(project_root)
+        if name in bohr_repo.tasks and not force:
+            logger.error(f"Task {name} is already defined")
+            exit(400)
+        try:
+            artifact_type = artifact_map[artifact]
+        except KeyError:
+            logger.error(f"Artifact not found: {artifact}")
+            exit(404)
+        label_list = list(map(lambda s: s.strip(), labels.split(",")))
+        if not use_all_datasets:
+            train_datasets, test_datasets = {}, {}
+        else:
+            all_datasets = {
+                n: d
+                for n, d in bohr_repo.datasets.items()
+                if d.artifact_type == artifact_type
+            }
+            train_datasets, test_datasets = train_and_test(all_datasets, label_column)
+        heuristic_groups = get_heuristic_module_list(
+            artifact_type, path_config.heuristics
+        )
+        task = Task(
+            name,
+            authors,
+            description,
+            artifact_type,
+            label_list,
+            train_datasets,
+            test_datasets,
+            label_column,
+            heuristic_groups,
+        )
+        bohr_repo.tasks[name] = task
+        bohr_repo.dump(project_root)
+        if repro:
+            logger.info("Re-running the pipeline ...")
+            api.repro(name, bohr_repo=bohr_repo)
