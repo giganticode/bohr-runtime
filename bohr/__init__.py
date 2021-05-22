8a4e4a2068df69b30881e92744e39f51d397a99f
hlib <hlibbabii@gmail.com>
2021-05-22 17:05:35 +0200

    log no warnings in non-verbose mode

--- bohr/__init__.py
+++ bohr/__init__.py
@@ -1,5 +1,8 @@
 import logging
 import os
+import sys
+import traceback
+import warnings
 from pathlib import Path
 from typing import Optional
 
@@ -36,10 +39,25 @@ def appname() -> str:
 __version__ = version()
 
 
+def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
+
+    print("============================")
+    log = file if hasattr(file, "write") else sys.stderr
+    traceback.print_stack(file=log)
+    log.write(warnings.formatwarning(message, category, filename, lineno, line))
+    print("============================")
+
+
 def setup_loggers(verbose: Optional[bool] = None):
     if verbose is None:
         verbose = AppConfig.load().verbose
-    logging.captureWarnings(True)
+    if not verbose:
+        logging.captureWarnings(True)
+        logging.getLogger("py.warnings").setLevel(logging.ERROR)
+    else:
+        import warnings
+
+        warnings.showwarning = warn_with_traceback
     root = logging.root
     for (logger_name, logger) in root.manager.loggerDict.items():
         if logger_name != "bohr" and not logger_name.startswith("bohr."):
