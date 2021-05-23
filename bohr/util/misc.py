72449b5d730e93a645a938df70d47732afc32665
hlib <hlibbabii@gmail.com>
2021-05-23 17:43:55 +0200

    refactor parse_labels and add tests

--- bohr/util/misc.py
+++ bohr/util/misc.py
@@ -1,4 +1,4 @@
-from typing import Set, Tuple, Union
+from typing import Any, Dict, List, Set, Tuple, Union
 
 import regex
 
@@ -14,3 +14,24 @@ def camel_case_to_snake_case(identifier: str) -> str:
         )
     ]
     return "_".join(parts).lower()
+
+
+def merge_dicts_(
+    a: Dict[str, List[Any]], b: Dict[str, List[Any]]
+) -> Dict[str, List[Any]]:
+    """
+    >>> a = {}
+    >>> merge_dicts_(a, {})
+    {}
+    >>> merge_dicts_(a, {'x': ['x1']})
+    {'x': ['x1']}
+    >>> merge_dicts_(a, {'x': ['x2']})
+    {'x': ['x1', 'x2']}
+    >>> merge_dicts_(a, {'x': ['x3'], 'y': ['y1']})
+    {'x': ['x1', 'x2', 'x3'], 'y': ['y1']}
+    """
+    for k, v in b.items():
+        if k not in a:
+            a[k] = []
+        a[k].extend(v)
+    return a
