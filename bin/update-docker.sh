ac281ea0e9bf614e47567022ad87f1b57dc820b1
hlibbabii <hlibbabii@gmail.com>
2022-08-12 21:07:14 +0300

    use docker run action

--- bin/update-docker.sh
+++ bin/update-docker.sh
@@ -6,9 +6,20 @@ base_ref="$1"
 
 if [ -z "$base_ref" ]; then
     base_ref="origin/master"
+    echo "base_ref not passed - comparing Dockerfile with default base_ref: $base_ref"
 fi
 
-if [ -n "$(git --no-pager diff $base_ref -- Dockerfile)" ]; then
+cmd="git --no-pager diff $base_ref -- Dockerfile"
+echo "Running command: $cmd"
+
+DOCKERFILE_DIFF="$(git --no-pager diff $base_ref -- Dockerfile)"
+
+if [ -n "$DOCKERFILE_DIFF" ]; then
+    echo "Dockerfile has changed"
+    echo "The change is the following:"
+    echo "----------------------------------------------------------"
+    echo "$DOCKERFILE_DIFF"
+    echo "----------------------------------------------------------"
     docker build --tag giganticode/bohr-cml-base:latest .
     docker push giganticode/bohr-cml-base:latest
 else
